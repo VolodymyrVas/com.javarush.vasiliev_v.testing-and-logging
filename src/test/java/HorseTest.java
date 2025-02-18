ab37fac4e4357bea57a4927bd2d0fbd5afeebed2
volodymyr <vvavodymyr@gmail.com>
2025-02-18 23:39:20 +0100

    добавлены два теста для конструктора класса Horse

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,2 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class HorseTest {
+
+    @Test
+    void testConstructorsThrowExceptionIfFirstParameterIsNull() {
+        // Проверяем оба конструктора в одном тесте
+        assertThrows(IllegalArgumentException.class, () -> new Horse(null, 2.2));
+        assertThrows(IllegalArgumentException.class, () -> new Horse(null, 2.2, 50.2));
+    }
+
+    @Test
+    void testConstructorsThrowExceptionWithCorrectMessageIfFirstParameterIsNull() {
+        // Проверяем второй конструктор так как у него есть выброс exceptions
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                                                           () -> new Horse(null, 7.7, 51.2)
+        );
+        assertEquals("Name cannot be null.", exception.getMessage());
+    }
+
+
+
+
 }
