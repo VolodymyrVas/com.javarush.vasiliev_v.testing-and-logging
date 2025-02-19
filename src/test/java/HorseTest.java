55f697e44b0646be65d875fdac790b0cd90eeebc
volodymyr <vvavodymyr@gmail.com>
2025-02-19 01:43:52 +0100

    покрытие тестами класса Horse его конструкторов завершено

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,12 +18,52 @@ public class HorseTest {
     void testConstructorsThrowExceptionWithCorrectMessageIfFirstParameterIsNull() {
         // Проверяем второй конструктор так как у него есть выброс exceptions
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-                                                           () -> new Horse(null, 7.7, 51.2)
+                                                          () -> new Horse(null, 7.7, 51.2)
         );
         assertEquals("Name cannot be null.", exception.getMessage());
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\t", "\n", "   ", "\r\n", "\r"})
+    void testConstructorThrowsExceptionForBlankStrings(String invalidName) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(invalidName, 2.2));
+        assertThrows(IllegalArgumentException.class, () -> new Horse(invalidName, 2.2, 50.2));
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\t", "\n", "   ", "\r\n", "\r"})
+    void testConstructorsThrowExceptionWithCorrectMessageIfFirstParameterIsBlankString(String invalidName) {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse(invalidName, 2.2, 50.2));
+        assertEquals("Name cannot be blank.", exception.getMessage());
+    }
 
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -10, -100, -999}) // Передаём разные отрицательные числа
+    void testConstructorThrowsExceptionForNegativeNumbers(int invalidNumber) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse("BlackBlade", invalidNumber));
+    }
 
+    @Test
+    void testConstructorsThrowExceptionWithCorrectMessageIfSecondParameterIsNegativeNumbers() {
+        // Проверяем второй конструктор так как у него есть выброс exceptions вот теперь точно есть ощущение что както это все можно пособирать в кучу!!!
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                                                          () -> new Horse("BlackBlade", -7.7, 51.2)
+        );
+        assertEquals("Speed cannot be negative.", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -10, -100, -999}) // Передаём разные отрицательные числа
+    void testConstructorThrowsExceptionForNegativeNumbersforThirdParameter(int invalidNumber) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse("BlackBlade", 5.5, invalidNumber));
+    }
+
+    @Test
+    void testConstructorsThrowExceptionWithCorrectMessageIfThirdParameterIsNegativeNumbers() {
+        // Проверяем второй конструктор так как у него есть выброс exceptions вот теперь точно есть ощущение что както это все можно пособирать в кучу!!!
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                                                          () -> new Horse("BlackBlade", 7.7, -51.2)
+        );
+        assertEquals("Distance cannot be negative.", exception.getMessage());
+    }
 }
